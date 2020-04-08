25b8734b1d9987447d963ffb82989159417968e5
way4ward17 <supersondultimate@gmail.com>
2020-04-08 22:22:19 +0100

    Adding Data types

---
+++ bin/main.dart
@@ -0,0 +1,58 @@
+main() {
+
+  //VARIABLE
+  String name = "Way4ward";
+  bool isBool = false;
+  double price = 9.000;
+  List fruit = ['Apple','Orange', 'Banana'];
+
+  var level = new Map<int,String>();
+  level = {1:"Nigeria",23:"South Africa"};
+
+
+  //VAR,CONST,FINAL
+
+  var baba = "water";
+  baba = "fish";
+  baba = "School";
+
+  //baba = 1 as String;
+
+  const babe = 1;
+
+  final firstName = "Ponmile";
+  final lastName = "Adefuwa";
+
+
+  //fullName = "Tobi";
+  //CONCATINATION
+int a = 1;
+int b = 2;
+int c = a + b;
+int age = c;
+
+ print("My firstname is $firstName and my lastname is $lastName  and i am $age year old");
+
+
+
+
+
+
+//  print(country);
+//  var a = 1;
+//
+// var fruitHolder = fruit[1];
+  Map country = {"NG":"Nigeria","SA":"South Africa"};
+  var countryHolder = country['NG'];
+//
+  print("Ponmile is $a year old $countryHolder" );
+
+//print(name.toUpperCase());
+//print(age);
+//print(isBool);
+//print(fruit[1]);
+//print(level[1]);
+//print(country["NG"]);
+
+
+}
